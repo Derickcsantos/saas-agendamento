442f0ade168fc4a2d682dc7c8163b281c526170c
Dérick Campos Santos <derickcampossantos1@gmail.com>
2025-11-05 11:30:52 -0300

    Adiciona o frontend no cors

--- backend/utils/corsOptions.js
+++ backend/utils/corsOptions.js
@@ -1,7 +1,7 @@
 
 
 export const corsOptions = {
-  origin: ['http://localhost:3000', 'https://ubiquitous-train-v6pw96wx6v64h664v-3000.app.github.dev', 'http://localhost:3001', 'https://marcafybackend.up.railway.app/'], 
+  origin: ['http://localhost:3000', 'https://ubiquitous-train-v6pw96wx6v64h664v-3000.app.github.dev', 'http://localhost:3001', 'https://marcafybackend.up.railway.app/', 'https://marcafy.vercel.app/'], 
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'organization-id', 'organization_id', 'Accept'],
   credentials: true,
