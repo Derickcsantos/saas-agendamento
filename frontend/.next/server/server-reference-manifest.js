2725d85b6182455b977cdd1316e2db4d6be08672
Dérick Campos Santos <derickcampossantos1@gmail.com>
2025-11-05 10:16:42 -0300

    Comita arquivos que falharam

--- frontend/.next/server/server-reference-manifest.js
+++ frontend/.next/server/server-reference-manifest.js
@@ -1,1 +1,5 @@
+<<<<<<< Updated upstream
 self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"OC+hO4SjNiV/KOyXOWcXV1pvq7FzclTfxvWKH7vOV4g=\"\n}"
+=======
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"IGO5MASo4sy26a5RYZQhEDNkC8xa+RnVMP8qS9VG2/8=\"\n}"
+>>>>>>> Stashed changes
