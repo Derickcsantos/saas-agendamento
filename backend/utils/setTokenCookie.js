ff2d1737ad1411ee9dbcb37291c121a575558701
Dérick Campos Santos <derickcampossantos1@gmail.com>
2025-11-25 09:34:42 -0300

    Atualiza definição do token para otimização do login

--- backend/utils/setTokenCookie.js
+++ backend/utils/setTokenCookie.js
@@ -1,8 +1,10 @@
 export default function setTokenCookie(res, token) {
   res.cookie('token', token, {
-    httpOnly: true,
-    secure: process.env.NODE_ENV === 'production', // em dev pode ser false
-    sameSite: 'strict',
-    maxAge: 60 * 60 * 1000, // 1 hora
-  });
+  httpOnly: true,
+  secure: true,
+  sameSite: 'none',   // <- ESSENCIAL
+  path: '/',
+  maxAge: 60 * 60 * 3 * 1000,
+});
+
 }
