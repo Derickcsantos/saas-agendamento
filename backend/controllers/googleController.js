a86f163c5beb09d56f32a453c6a81ff4beebd024
Dérick Campos Santos <164596759+Derickcsantos@users.noreply.github.com>
2025-12-08 18:16:41 -0300

    Update googleController.js

--- backend/controllers/googleController.js
+++ backend/controllers/googleController.js
@@ -6,7 +6,7 @@ export const googleCallback = async (req, res) => {
   const user = req.user;
 
   if (!user) {
-    return res.redirect("/login?error=unauthorized");
+    return res.redirect(`${process.env.FRONTEND_URL}/login?error=unauthorized`);
   }
 
   // Buscar slug da organização
