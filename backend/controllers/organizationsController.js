4cd6455ef79cb7cbd819e6df53cfc2d0d19b89af
Dérick Campos Santos <164596759+Derickcsantos@users.noreply.github.com>
2025-12-10 02:51:46 +0000

    Corrige erro para envio de emails de boas vindas

--- backend/controllers/organizationsController.js
+++ backend/controllers/organizationsController.js
@@ -214,7 +214,7 @@ export async function createOrganization(req, res) {
         await brevo.sendTransacEmail({
           to: [{email, name}],
           sender: { email: 'marcafy.ofc@gmail.com', name: 'Marcafy' },
-          template: 1,
+          templateId: 1,
           params: {
             organizationName: name,
             email: email
